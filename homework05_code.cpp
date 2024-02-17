47012858326879cd0600cd4ab8397fe52a6bc657
TJ-Liebsch <148586792+TJ-Liebsch@users.noreply.github.com>
2024-02-16 21:53:37 -0600

    Update homework05_code.cpp

--- homework05_code.cpp
+++ homework05_code.cpp
@@ -13,7 +13,7 @@ int players = 6;
 // These are the function prototypes
 int readFile(string[], string[], int[]);
 string whoWon(string[], int[]);
-void printWinner(string[], string[], int[], string);
+
 
 int main() // The main function just creates the arrays and calls the 3 functions
 {
